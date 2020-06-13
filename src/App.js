1147403c02df280af86e8fb8484c601b2df5e69c
Imade Osifo <toxsik88@gmail.com>
2020-06-12 21:58:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
+        <div>Welcome to Defund to Refund!!!</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
